7e9d18f5db818a7ecdae03bbb41bfb6633941e2c
prathiparaj <prathiparaj1994@gmail.com>
2018-01-02 17:41:03 +0530

    dgdhncncgsbz

--- Priority1(MphasisTadam)/Toad-Edge/Toad-Edge.ps1
+++
@@ -1,4 +0,0 @@
-$uri = "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/ToadEdge.msi"
-$out = "c:\ToadEdge.msi"
-Invoke-WebRequest -uri $uri -OutFile $out
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $out /quiet /norestart /l c:\installlog.txt"  
