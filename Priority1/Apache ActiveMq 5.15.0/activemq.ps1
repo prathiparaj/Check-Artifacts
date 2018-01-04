7abe507c691885eb366e23aee10fe6e701efdf7d
prathiparaj <prathiparaj1994@gmail.com>
2018-01-04 19:21:04 +0530

    asdfgh

--- Priority1/Apache ActiveMq 5.15.0/activemq.ps1
+++ Priority1/Apache ActiveMq 5.15.0/activemq.ps1
@@ -1,101 +1,48 @@
-﻿Param(
-    [ValidateNotNullOrEmpty()]
-    [string]
-    $RawPackagesList="activemq"
-)
+﻿function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Software-Modules"
+Create-Folder "$setupFolder"
+
+#Create-Folder "c:\Spring-Framework"
+
+
+
+
+if((Test-Path "$setupFolder\intellijidea-community.bat") -eq $false)
+{
   
-$ErrorActionPreference = "Stop"
- 
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
-$ChocolateyPackageInstallerFolder = Join-Path $env:ALLUSERSPROFILE -ChildPath $("ChocolateyPackageInstaller-" + [System.DateTime]::Now.ToString("yyyy-MM-dd-HH-mm-ss"))
-$ScriptLog = Join-Path -Path $ChocolateyPackageInstallerFolder -ChildPath "ChocolateyPackageInstaller.log"
-$ChocolateyInstallLog = Join-Path -Path $ChocolateyPackageInstallerFolder -ChildPath "ChocolateyInstall.log"
-function DisplayArgValues
-{
-    WriteLog '========== Configuration =========='
-    WriteLog "RawPackagesList : $RawPackagesList"
-    WriteLog '========== Configuration =========='
-}
-function InitializeFolders
-{
-    if ($false -eq (Test-Path -Path $ChocolateyPackageInstallerFolder))
-    {
-        New-Item -Path $ChocolateyPackageInstallerFolder -ItemType directory | Out-Null
-    }
+        Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/intellijidea-community.bat" "$setupFolder\intellijidea-community.bat"  
 }
 
-function WriteLog
-{
-    Param(
-        
-        [string]$Message,
-        [switch]$LogFileOnly
-    )
-    $timestampedMessage = "[$([System.DateTime]::Now)] $Message" | % {
-        if (-not $LogFileOnly)
-        {
-            Write-Host -Object $_
-        }
-        Out-File -InputObject $_ -FilePath $ScriptLog -Append
-    }
-}
-function InstallChocolatey
-{
-    Param(
-        [ValidateNotNullOrEmpty()] $chocolateyInstallLog
-    )
-    WriteLog 'Installing Chocolatey ...'
-    Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) | Out-Null
-    WriteLog 'Success.'
-}
+Start-Process -FilePath $setupFolder\intellijidea-community.bat -Wait
 
-function InstallPackages
-{
-    Param(
-        [ValidateNotNullOrEmpty()][string] $packagesList
-    )
-    $separator = @(";",",")
-    $splitOption = [System.StringSplitOptions]::RemoveEmptyEntries
-    $packages = $packagesList.Trim().Split($separator, $splitOption)
-    if (0 -eq $packages.Count)
-    {
-        WriteLog 'No packages were specified. Exiting.'
-        return        
-    }
-    foreach ($package in $packages)
-    {
-        $package = $package.Trim()
-        WriteLog "Installing package: $package ..."
-        choco install $package --version 5.10.0 --force --yes --acceptlicense --verbose --allow-empty-checksums | Out-Null  
-        if (-not $?)
-        {
-            $errMsg = 'Installation failed. Please see the chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details.'
-            throw $errMsg 
-        }
-    
-        WriteLog 'Success.'
-    }
-}
 
-try
-{
-    #
-    InitializeFolders
-    #
-    DisplayArgValues
-    
-    # install the chocolatey package manager
-    InstallChocolatey -chocolateyInstallLog $ChocolateyInstallLog
-    # install the specified packages
-    InstallPackages -packagesList $RawPackagesList
-}
-catch
-{
-    $errMsg = $Error[0].Exception.Message
-    if ($errMsg)
-    {
-        WriteLog -Message "ERROR: $errMsg" -LogFileOnly
-    }
-    throw
-}
- 
+#$env:Path += ";C:\mysql-5.7.20-winx64\bin"
+
+#mysqld --initialize
+#mysqld
+#& cmd /c  'mysqld --initialize' 
+#& cmd /c  'mysqld'
