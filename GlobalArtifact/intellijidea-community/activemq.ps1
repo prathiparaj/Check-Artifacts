8186eb0867d607fbf5f4124885db4b58467f5268
prathiparaj <33244470+prathiparaj@users.noreply.github.com>
2018-01-04 18:39:16 +0530

    Add files via upload

---
+++ GlobalArtifact/intellijidea-community/activemq.ps1
@@ -0,0 +1,48 @@
+﻿function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Software-Modules"
+Create-Folder "$setupFolder"
+
+#Create-Folder "c:\Spring-Framework"
+
+
+
+
+if((Test-Path "$setupFolder\intellijidea-community.bat") -eq $false)
+{
+  
+        Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/intellijidea-community.bat" "$setupFolder\intellijidea-community.bat"  
+}
+
+Start-Process -FilePath $setupFolder\intellijidea-community.bat -Wait
+
+
+#$env:Path += ";C:\mysql-5.7.20-winx64\bin"
+
+#mysqld --initialize
+#mysqld
+#& cmd /c  'mysqld --initialize' 
+#& cmd /c  'mysqld'
