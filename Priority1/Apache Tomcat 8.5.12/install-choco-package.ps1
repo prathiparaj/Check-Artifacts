d914b7da46200a00c5d78e9a66abe6d69e730722
prathiparaj <prathiparaj1994@gmail.com>
2018-01-08 13:51:05 +0530

    tomcat

--- Priority1/Apache Tomcat 8.5.12/install-choco-package.ps1
+++ Priority1/Apache Tomcat 8.5.12/install-choco-package.ps1
@@ -1,7 +1,7 @@
 ﻿[CmdletBinding()]
 param(
     # Space-, comma- or semicolon-separated list of Chocolatey packages.
-    [string] $Packages="activemq",
+    [string] $Packages="tomcat",
 
     # Minimum PowerShell version required to execute this script.
     [int] $PSVersionRequired = 3
