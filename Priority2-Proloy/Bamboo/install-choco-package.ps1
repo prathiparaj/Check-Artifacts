d38a854aea7c93df47df3373d722d7224e5d55de
prathiparaj <prathiparaj1994@gmail.com>
2018-01-09 21:31:22 +0530

    zxcvbnm,asdfghjklqwertyuio9876543`1234567890

---
+++ Priority2-Proloy/Bamboo/install-choco-package.ps1
@@ -0,0 +1,43 @@
+﻿function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Software-Modules"
+Create-Folder $setupFolder
+
+
+
+#download sonar file
+if((Test-Path "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe") -eq $false)
+{
+  
+        Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/atlassian-bamboo-6.3.0-windows-x64.exe" "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe"  
+}
+
+cmd.exe $setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe -q	
+
+
+
+  #Remove Zip file
+Remove-Item –Path "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe" -Recurse
