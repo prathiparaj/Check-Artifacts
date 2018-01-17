3a5e0813d9d44b0cf6f6d6256f8e05c546161a87
prathiparaj <33244470+prathiparaj@users.noreply.github.com>
2018-01-17 12:38:14 +0530

    Update install-choco-package.ps1

--- Priority2-Proloy/Bamboo/install-choco-package.ps1
+++ Priority2-Proloy/Bamboo/install-choco-package.ps1
@@ -41,38 +41,22 @@ if((Test-Path "$setupFolder\Bamboo.bat") -eq $false)
         Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/Bamboo.bat" "$setupFolder\Bamboo.bat"  
 }
 # Install Bamboo
-Start-Process -FilePath "$setupFolder\Bamboo.bat"
+Start-Process -FilePath "$setupFolder\Bamboo.bat" 
 Start-Sleep -s 50
-
 
 
 # Install Bamboo as Service
 Start-Process -FilePath "C:\Program Files\Bamboo\InstallAsService.bat" 
-#Start-Sleep -s 10
-
-# Start Bamboo Service
-Start-Process -FilePath "C:\Program Files\Bamboo\bin\start-bamboo.bat"
-
-sc config bamboo start='auto'
-
-net start bamboo
-#Start-Sleep -s 10
-#Start-Process -FilePath "C:\Program Files\Bamboo\bin\start-bamboo.bat"
-#Start-Sleep -s 10
-
+Start-Sleep -s 10
 
 
 # Start Bamboo Service
-#Start-Process -FilePath "C:\Program Files\Bamboo\StartBamboo.bat"
-#Start-Sleep -s 10
-
-# Start Bamboo Console
-#Start-Process -FilePath "C:\Program Files\Bamboo\BambooConsole.bat"
-#Start-Sleep -s 10
+Start-Process -FilePath "C:\Program Files\Bamboo\StartBamboo.bat"
 
 
- #Remove exe
+
+#Remove exe
 Remove-Item –Path "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe"
 
 #Remove exe
-Remove-Item –Path "$setupFolder\Bamboo.bat" 
+Remove-Item –Path "$setupFolder\Bamboo.bat"  
