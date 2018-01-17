aa7e41dfd9051fb9dd49b4991165ff0c41a82743
prathiparaj <33244470+prathiparaj@users.noreply.github.com>
2018-01-17 18:21:08 +0530

    Update install-choco-package.ps1

--- Priority2-Proloy/Bamboo/install-choco-package.ps1
+++ Priority2-Proloy/Bamboo/install-choco-package.ps1
@@ -1,3 +1,63 @@
-$setupFolder = "c:\Software-Modules"
+function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
 
-Start-Process -FilePath "$setupFolder\StartBamboo.bat"
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Software-Modules"
+Create-Folder $setupFolder
+
+
+
+#download Bamboo exe
+if((Test-Path "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe") -eq $false)
+{
+  
+        Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/atlassian-bamboo-6.3.0-windows-x64.exe" "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe"  
+}
+
+if((Test-Path "$setupFolder\Bamboo.bat") -eq $false)
+{
+  
+        Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/Bamboo.bat" "$setupFolder\Bamboo.bat"  
+}
+# Install Bamboo
+Start-Process -FilePath "$setupFolder\Bamboo.bat" 
+Start-Sleep -s 50
+
+
+# Install Bamboo as Service
+Start-Process -FilePath "C:\Program Files\Bamboo\InstallAsService.bat" 
+Start-Sleep -s 10
+
+
+# Start Bamboo Service
+Start-Process -FilePath "C:\Program Files\Bamboo\StartBamboo.bat"
+
+
+
+#Remove exe
+Remove-Item –Path "$setupFolder\atlassian-bamboo-6.3.0-windows-x64.exe"
+
+#Remove exe
+Remove-Item –Path "$setupFolder\Bamboo.bat"  
+
