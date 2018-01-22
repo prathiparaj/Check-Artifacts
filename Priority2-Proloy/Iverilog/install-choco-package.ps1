87bc77779690fbd3c9ae0a21193fc43f3fa9847f
prathiparaj <prathiparaj1994@gmail.com>
2018-01-22 13:33:33 +0530

    gvlkbxjashxlaxlaxhlklaxkahxlxlkblkbviogvkjgiogojhtoufugi

---
+++ Priority2-Proloy/Iverilog/install-choco-package.ps1
@@ -0,0 +1,60 @@
+﻿
+
+
+function Create-Folder {
+    Param ([string]$path)
+    if ((Test-Path $path) -eq $false) 
+    {
+        Write-Host "$path doesn't exist. Creating now.."
+        New-Item -ItemType "directory" -Path $path
+    }
+}
+
+function Download-File{
+    Param ([string]$src, [string] $dst)
+
+    (New-Object System.Net.WebClient).DownloadFile($src,$dst)
+    #Invoke-WebRequest $src -OutFile $dst
+}
+
+function WaitForFile($File) {
+  while(!(Test-Path $File)) {    
+    Start-Sleep -s 10;   
+  }  
+} 
+
+
+#Setup Folders
+
+$setupFolder = "c:\Software-Modules"
+Create-Folder $setupFolder
+
+
+
+#download sonar file
+if((Test-Path "$setupFolder\iverilog-10-1-1-x64_setup.exe") -eq $false)
+{
+  
+        Download-File "https://globalartifactstg.blob.core.windows.net/globalsoftwarelink4artifact/iverilog-10-1-1-x64_setup.exe" "$setupFolder\iverilog-10-1-1-x64_setup.exe"  
+}
+
+
+
+Start-Process -FilePath "$setupFolder\iverilog-10-1-1-x64_setup.exe" -ArgumentList "/verysilent" -Wait
+
+
+#Remove Zip file
+Remove-Item –Path "$setupFolder\iverilog-10-1-1-x64_setup.exe" -Recurse
+	
+
+
+
+
+
+
+
+
+
+
+
+
